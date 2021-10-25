08d5b1fb9632681873971082ac05ce8387b161ef
Flora Santana <39812840+Floraluiza@users.noreply.github.com>
2021-10-25 01:04:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// Crie um programa que armazene seu nome em uma variável e imprima a mensagem "Olá, [nome]" utilizando Template String
+
+var nome = "Flora"
+console.log("Olá, " + nome + ".")
+
+// Crie um programa com duas variáveis do tipo Double e imprima a soma das mesmas
+
+var n1 = 7.5
+var n2 = 5.5
+console.log(n1+n2)
+
+//Crie um programa que imprima o valor ao quadrado de um número
+
+Math.pow(7, 2);
+
+//Crie um programa que imprima o dobro de um número
+
+function dobroDoProximo(atual){
+   return (atual+1)*2;
+}
+console.log(dobroDoProximo(4));
+
+//Crie um programa que utilize os operadores aritméticos e imprima o antecessor e o sucessor de um número
+
+
+
+// Crie um programa que calcule e imprima o total de dias em 7 meses, considerando que cada mês tenha 30 dias
+
+
+// Crie um programa que calcule e imprima a média das cinco notas de um aluno
+
+
+// Crie um programa que converta metros para centímetros e imprima o resultado
+
+
+// Crie um programa que calcule o desconto na venda de um produto
